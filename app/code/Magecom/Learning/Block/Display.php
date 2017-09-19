a755e0820fdc7dbd2ae7bf38eeed84b8dad48e62
user <peers.rom@gmail.com>
2017-09-19 14:36:53 +0300

    task 4 is done

---
+++ app/code/Magecom/Learning/Block/Display.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Magecom\Learning\Block;
+
+use \Magento\Framework\View\Element\Template\Context;
+
+class Display extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(Context $context)
+    {
+        parent::__construct($context);
+    }
+
+}
---
+++ app/code/Magecom/Learning/Controller/Front/Index.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Magecom\Learning\Controller\Front;
+
+use Magento\Framework\App\Action\Context;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+
+    public function __construct(Context $context)
+    {
+        parent::__construct($context);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute()
+    {
+        $resultPage	= $this->resultFactory->create(\Magento\Framework\Controller\ResultFactory::TYPE_PAGE);
+
+        return	$resultPage;
+    }
+}

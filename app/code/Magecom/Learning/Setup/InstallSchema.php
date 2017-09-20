6e33450dc9c3855a6f6c6ce2ccee1703ea1f7f25
user <peers.rom@gmail.com>
2017-09-20 12:17:18 +0300

    task 5 is done

---
+++ app/code/Magecom/Learning/Setup/InstallSchema.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Magecom\Learning\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+
+        $installer->startSetup();
+
+        $table = $installer->getConnection()->newTable(
+            $installer->getTable('learning_items')
+        )->addColumn(
+            'item_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            null,
+            ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+            'Item ID'
+        )->addColumn(
+            'title',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            ['nullable' => false],
+            'Title'
+        )->addColumn(
+            'content',
+            \Magento\Framework\DB\Ddl\Table::TYPE_VARBINARY,
+            '1m',
+            ['nullable' => false],
+            'Content'
+        )->addColumn(
+            'url_key',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            ['nullable' => false],
+            'Url key'
+        )->addColumn(
+            'creation_time',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+            'Creation time'
+        )->addColumn(
+            'update_time',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
+            'Updated time'
+        );
+        $installer->getConnection()->createTable($table);
+
+        $installer->endSetup();
+
+    }
+}

deaef27ba565ad3e07b8120a372557cea86508a6
user <peers.rom@gmail.com>
2017-09-28 14:10:44 +0300

    task 11 is done

--- app/code/Magecom/Learning/Controller/Adminhtml/Items/Index.php
+++ app/code/Magecom/Learning/Controller/Adminhtml/Items/Index.php
@@ -10,8 +10,12 @@ class Index extends \Magecom\Learning\Controller\Adminhtml\Items
      */
     public function execute()
     {
+        \Magento\Framework\Profiler::start(__CLASS__ . '::' . __METHOD__);
+
         $this->_initAction();
         $this->_view->getPage()->getConfig()->getTitle()->prepend(__('Items'));
         $this->_view->renderLayout();
+
+        \Magento\Framework\Profiler::stop(__CLASS__ . '::' . __METHOD__);
     }
 }

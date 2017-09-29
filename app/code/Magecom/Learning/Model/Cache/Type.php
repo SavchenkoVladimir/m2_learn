b0617f2bde84aa84cd7b5f72f99d8166e281567a
user <peers.rom@gmail.com>
2017-09-29 18:19:04 +0300

    custom cache type is added

---
+++ app/code/Magecom/Learning/Model/Cache/Type.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Magecom\Learning\Model\Cache;
+
+class Type extends \Magento\Framework\Cache\Frontend\Decorator\TagScope
+{
+    const TYPE_IDENTIFIER = 'learning_cache_id';
+    const CACHE_TAG = 'LEARNING_CACHE_TAG';
+
+    public function __construct(\Magento\Framework\App\Cache\Type\FrontendPool $cacheFrontendPool)
+    {
+        parent::__construct($cacheFrontendPool->get(self::TYPE_IDENTIFIER), self::CACHE_TAG);
+    }
+}
